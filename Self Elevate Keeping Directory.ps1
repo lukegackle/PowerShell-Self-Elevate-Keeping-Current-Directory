2d9b1681ba8b5fa21c8b2737584b734778f9d1ce
lukegackle <33621585+lukegackle@users.noreply.github.com>
2019-06-21 14:12:21 +0930

    Initial Upload of Script

---
+++ Self Elevate Keeping Directory.ps1
@@ -0,0 +1,36 @@
+﻿Param(
+    [string]$Loc
+)
+
+$Delay = 2
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
+        [Security.Principal.WindowsBuiltInRole] 'Administrator')
+)
+{
+    Write-Host "Not elevated, restarting in $Delay seconds ..."
+    $Loc = Get-Location
+    Start-Sleep -Seconds $Delay
+
+    $Arguments =  @(
+        '-NoProfile',
+        '-ExecutionPolicy Bypass',
+        '-NoExit',
+        '-File',
+        "`"$($MyInvocation.MyCommand.Path)`"",
+        "\`"$Loc\`""
+    )
+    Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $Arguments
+    Break
+}
+else
+{
+    Write-Host "Already elevated, exiting in $Delay seconds..."
+    Start-Sleep -Seconds $Delay
+}
+if($Loc.Length -gt 1){
+Set-Location $Loc.Substring(1,$Loc.Length-1)
+}
+
+#Your Script Here...
+Read-Host "Done.."
